20c72a81b93e2772888c1f4be254a12e25d5dfef
willsather <will@sather.ws>
2025-02-17 18:03:06 -0500

    fix: post list item link

--- src/app/(components)/post-list-item.tsx
+++ src/app/(components)/post-list-item.tsx
@@ -31,52 +31,52 @@ export async function PostListItem({
   const showInBrief = await inBriefFlag();
 
   return (
-    <article
-      className={`group flex gap-6 border-b py-6 first:pt-0 ${styles.borderColor}`}
-    >
-      {/* Thumbnail */}
+    <article>
       <Link
         href={`/posts/${post.slug}`}
-        className="relative block aspect-[563/375] w-[150px] shrink-0 overflow-hidden bg-gray-500"
+        className={`group flex gap-6 border-b py-6 first:pt-0 ${styles.borderColor}`}
       >
-        {post.image !== "" ? (
-          <img
-            src={post.image ?? "/default.png"}
-            alt={post.image}
-            className={`h-full w-full object-cover transition-transform duration-300 ${styles.hoverScale}`}
-          />
-        ) : (
-          <div className="h-full w-full bg-gray-500" />
-        )}
+        {/* Thumbnail */}
+        <div className="relative block aspect-[563/375] w-[150px] shrink-0 overflow-hidden bg-gray-500">
+          {post.image !== "" ? (
+            <img
+              src={post.image ?? "/default.png"}
+              alt={post.image}
+              className={`h-full w-full object-cover transition-transform duration-300 ${styles.hoverScale}`}
+            />
+          ) : (
+            <div className="h-full w-full bg-gray-500" />
+          )}
+        </div>
+
+        {/* Content */}
+        <div className="flex flex-col">
+          <div>
+            <div
+              className={`mb-2 inline-block font-medium text-xs ${styles.textColor}`}
+            >
+              {post.categories?.[0]?.toUpperCase()}
+            </div>
+
+            <div className="flex items-start gap-2">
+              {showInBrief && post.metadata?.isBrief ? (
+                <div className="bg-tc-green p-1">
+                  <InBriefIcon className="size-4" />
+                </div>
+              ) : null}
+              <h2 className="mb-2 font-bold text-lg leading-tight tracking-tight hover:underline">
+                {post.title}
+              </h2>
+            </div>
+          </div>
+
+          <div className="mt-auto flex flex-col items-start gap-2 font-medium text-gray-500 text-sm md:flex-row md:items-center">
+            <span>{post.author}</span>
+            <span className="hidden md:block">-</span>
+            <time>{formatTimeSince(post.date)}</time>
+          </div>
+        </div>
       </Link>
-
-      {/* Content */}
-      <div className="flex flex-col">
-        <div>
-          <div
-            className={`mb-2 inline-block font-medium text-xs ${styles.textColor}`}
-          >
-            {post.categories?.[0]?.toUpperCase()}
-          </div>
-
-          <div className="flex items-start gap-2">
-            {showInBrief && post.metadata?.isBrief ? (
-              <div className="bg-tc-green p-1">
-                <InBriefIcon className="size-4" />
-              </div>
-            ) : null}
-            <h2 className="mb-2 font-bold text-lg leading-tight tracking-tight hover:underline">
-              {post.title}
-            </h2>
-          </div>
-        </div>
-
-        <div className="mt-auto flex flex-col items-start gap-2 font-medium text-gray-500 text-sm md:flex-row md:items-center">
-          <span>{post.author}</span>
-          <span className="hidden md:block">-</span>
-          <time>{formatTimeSince(post.date)}</time>
-        </div>
-      </div>
     </article>
   );
 }
