21d08c1e9cdbe3b8f45a24fc8012ca6ae4b76da5
willsather <will@sather.ws>
2025-02-17 17:04:21 -0500

    fix: ts import

--- src/app/.well-known/vercel/flags/route.ts
+++ src/app/.well-known/vercel/flags/route.ts
@@ -2,7 +2,7 @@ import { type ApiData, verifyAccess } from "@vercel/flags";
 import { getProviderData } from "@vercel/flags/next";
 import { type NextRequest, NextResponse } from "next/server";
 
-import * as flags from "@/lib/flags";
+import * as flags from "../../../../lib/flags";
 
 export async function GET(request: NextRequest) {
   const access = await verifyAccess(request.headers.get("Authorization"));
