a943d028aa2a5e70826f96a3af028f18e3055a03
willsather <will@sather.ws>
2025-02-18 19:41:23 -0500

    fix: hero separator skeleton

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -1,6 +1,7 @@
 import HeroSkeleton from "@/app/(components)/(skeletons)/hero-skeleton";
 import LatestPostsSkeleton from "@/app/(components)/(skeletons)/latest-posts-skeleton";
 import VenturePostsSkeleton from "@/app/(components)/(skeletons)/venture-posts-skeleton";
+import Separator from "@/app/(components)/separator";
 import TitleSection from "@/app/(home)/title";
 import UpcomingEvents from "@/app/(home)/upcoming-events";
 
@@ -10,6 +11,8 @@ export default function HomeLoadingPage() {
       <TitleSection />
 
       <HeroSkeleton />
+
+      <Separator />
 
       <main>
         <LatestPostsSkeleton />
